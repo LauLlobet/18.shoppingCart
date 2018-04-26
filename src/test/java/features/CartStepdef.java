0aec7275ce149dc2fd2d8d90e9665bc3aa6959ff
Lau Llobet <lau.llobet@gmail.com>
2018-04-26 16:46:05 +0200

    FEATURES REFLECT MORE THE DOMAIN REQUIREMENTS

--- src/test/java/features/CartStepdef.java
+++ src/test/java/features/CartStepdef.java
@@ -3,7 +3,7 @@ package features;
 import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
-import org.junit.Before;
+import cucumber.api.java.en.When;
 import shopping.Basket;
 import shopping.BasketRepository;
 import shopping.BasketService;
@@ -16,31 +16,32 @@ public class CartStepdef {
     private BasketService basketService;
     private Basket tempBasket;
     private ItemRepository itemRepository;
+    private String user;
 
-    @Given("^a shopping cart service")
-    public void aShoppingCart() throws Throwable {
-        itemRepository = new ItemRepository();
-        basketService = new BasketService(new BasketRepository(), itemRepository);
-    }
-
-    @Given("^I add (\\d+) units of \"([^\"]*)\" to the cart of \"([^\"]*)\"$")
-    public void iAddUnitsOfToTheCartOf(int amount, String itemId, String user) throws Throwable {
+    @Given("^I add (\\d+) units of \"([^\"]*)\" to the shopping basket$")
+    public void iAddUnitsOfToTheCartOf(int amount, String itemId) throws Throwable {
         basketService.addItem(user,itemId,amount);
     }
 
-    @Then("^I get the basket of \"([^\"]*)\"$")
-    public void iGetTheBasketOf(String user) throws Throwable {
+    @When("^I check the content of the shopping basket$")
+    public void iGetTheBasketOf() throws Throwable {
         tempBasket = basketService.basketFor(user);
     }
 
-
-    @Then("^the basket price is (\\d+)$")
+    @Then("^it contains the following price:(\\d+)$")
     public void theBasketPriceIs(int price) throws Throwable {
         assertThat(tempBasket.getPriceAmount(),is(price));
     }
 
-    @Given("^the inventory has item \"([^\"]*)\" at (\\d+) euros$")
+    @Given("^a \"([^\"]*)\" product is avaliable at (\\d+) euros$")
     public void thatTheDatabaseHasItemAtEuros(String itemId, int price) throws Throwable {
         itemRepository.add(itemId,price);
+    }
+
+    @Given("^a user of a shopping basket \"([^\"]*)\"$")
+    public void aUserOfAShoppingBasket(String user) throws Throwable {
+        itemRepository = new ItemRepository();
+        basketService = new BasketService(new BasketRepository(),itemRepository);
+        this.user = user;
     }
 }
--- src/test/resources/features/cart.feature
+++ src/test/resources/features/cart.feature
@@ -1,25 +1,22 @@
 Feature: Cart
+
+  Background:
+
+    Given a user of a shopping basket "John"
+    And a "The Hobbit" product is avaliable at 5 euros
+    And a "Breaking Bad" product is avaliable at 7 euros
+
   Scenario Outline: Items are added to the shopping cart
 
-    Given a shopping cart service
-    And the inventory has item "The Hobbit" at 5 euros
-    And the inventory has item "Breaking Bad" at 7 euros
-
-    When I add <hobbits> units of "The Hobbit" to the cart of "John"
-    And I add <bb> units of "Breaking Bad" to the cart of "John"
-
-    And I get the basket of "John"
-    Then the basket price is <price>
+    Given I add <hobbitunits> units of "The Hobbit" to the shopping basket
+    And I add <breakingbadunits> units of "Breaking Bad" to the shopping basket
+    When I check the content of the shopping basket
+    Then it contains the following price:<price>
 
     Examples:
-      | hobbits | bb  | price |
-      |   0     |  0  |  0    |
-      |   0     |  2  |  14   |
-      |   1     |  0  |  5    |
-      |   2     |  5  |  45   |
+      | hobbitunits | breakingbadunits  | price |
+      |   0          |  0               |  0    |
+      |   0          |  2               |  14   |
+      |   1          |  0               |  5    |
+      |   2          |  5               |  45   |
 
-  Scenario:
-
-    Given a shopping cart service
-    When I get the basket of "John"
-    Then the basket price is 0
